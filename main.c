be0e3fd3aa7cf834e7ea0fc67b5cb4c7dd8bf895
McLaren95 <maksimkruglikov1@gmail.com>
2024-02-24 17:44:39 +0300

    append access functions

--- main.c
+++ main.c
@@ -34,12 +34,50 @@ void test_popBack_notEmptyVector() {
     assert(v.capacity == 1);
 }
 
+void test_atVector_notEmptyVector() {
+    vector v = createVector(5);
+    pushBack(&v, 10);
+    pushBack(&v, 7);
+    pushBack(&v, 8);
+    pushBack(&v, 9);
+    pushBack(&v, 15);
+    int *index = atVector(&v, 3);
+    assert(*index == 9);
+}
+
+void test_atVector_requestToLastElement(){
+    vector v = createVector(5);
+    pushBack(&v, 10);
+    pushBack(&v, 7);
+    pushBack(&v, 8);
+    pushBack(&v, 9);
+    pushBack(&v, 15);
+    int *index = atVector(&v, 4);
+    assert(*index == 15);
+}
+
+void test_back_oneElementInVector(){
+    vector v = createVector(1);
+    pushBack(&v, 10);
+    int *index = back(&v);
+    assert(*index == 10);
+}
+void test_front_oneElementInVector(){
+    vector v = createVector(1);
+    pushBack(&v, 10);
+    int *index = front(&v);
+    assert(*index == 10);
+}
+
 void test() {
     test_pushBack_emptyVector();
     test_pushBack_fullVector();
-    test_popBack_emptyVector();
+    //test_popBack_emptyVector();
     test_popBack_notEmptyVector();
-
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
 }
 
 int main() {
