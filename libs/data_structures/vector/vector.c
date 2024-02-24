57123109595a5f1e456e618e82fb734216196197
McLaren95 <maksimkruglikov1@gmail.com>
2024-02-24 16:37:55 +0300

    append push / pop functions

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -12,7 +12,7 @@ vector createVector(size_t n) {
 void reserve(vector *v, size_t newCapacity) {
     if (newCapacity == 0) {
         free(v->data);
-    }else{
+    } else {
         int *newData = realloc(v->data, newCapacity * sizeof(int));
         if (newData == NULL) {
             fprintf(stderr, "bad alloc");
@@ -20,21 +20,50 @@ void reserve(vector *v, size_t newCapacity) {
         } else {
             if (newCapacity <= v->size)
                 v->size = newCapacity;
-            free(v->data);
             v->data = newData;
             v->capacity = newCapacity;
         }
     }
 }
 
-void clear(vector *v){
+
+void clear(vector *v) {
     v->size = 0;
 }
 
-void shrinkToFit(vector *v){
-    reserve(v,v->size);
+void shrinkToFit(vector *v) {
+    reserve(v, v->size);
 }
 
-void deleteVector(vector *v){
+void deleteVector(vector *v) {
     free(v->data);
+}
+
+bool isEmpty(vector *v) {
+    return v->size == 0;
+}
+
+bool isFull(vector *v) {
+    return v->size == v->capacity;
+}
+
+int getVectorValue(vector *v, size_t i) {
+    return i < v->size ? v->data[i] : -1;
+}
+
+void pushBack(vector *v, int x) {
+    if (isFull(v)) {
+        reserve(v, v->capacity == 0 ? 1 : v->capacity * 2);
+    }
+    v->data[v->size++] = x;
+}
+
+void popBack(vector *v) {
+    if (isEmpty(v)) {
+        fprintf(stderr, "vector is empty");
+        exit(1);
+    } else {
+        v->size--;
+        shrinkToFit(v);
+    }
 }
--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct vector {
     int *data; // указатель на элементы вектора
@@ -24,5 +25,20 @@ void shrinkToFit(vector *v);
 
 //освобождает память, выделенную вектору.
 void deleteVector(vector *v);
+
+// проверка на то, является ли вектор пустым.
+bool isEmpty(vector *v);
+
+// проверка на то, что вектор заполнен.
+bool isFull(vector *v);
+
+// возвращает i-ый элемент вектора v.
+int getVectorValue(vector *v, size_t i);
+
+//добавляет элемент x в конец вектора v.
+void pushBack(vector *v, int x);
+
+//удаляет последний элемент из вектора.
+void popBack(vector *v);
 
 #endif
