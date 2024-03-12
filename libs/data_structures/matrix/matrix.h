2a37b2a1069164b4026fe31efaa5fa3dacfbf6bd
McLaren95 <maksimkruglikov1@gmail.com>
2024-03-12 13:26:26 +0300

    structure declaration

---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,18 @@
+#ifndef INC_MATRIX_H
+#define INC_MATRIX_H
+
+#include <stdlib.h>
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+
+#endif
--- main.c
+++ main.c
@@ -1,86 +1,13 @@
 #include <stdio.h>
-#include <stdint.h>
-#include "libs/data_structures/vector/vector.h"
-#include <assert.h>
+#include <stdlib.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    int *a = (int*)malloc(N);
+    for(int i = 0; i<N;i++){
+        a[i]=i;
+    }
+    printf("NORMAl");
 
-void test_pushBack_emptyVector() {
-    vector v = createVector(0);
-    pushBack(&v, 5);
-    assert(v.size == 1);
+    return 0;
 }
-
-void test_pushBack_fullVector() {
-    vector v = createVector(3);
-    pushBack(&v, 5);
-    pushBack(&v, 1);
-    pushBack(&v, 6);
-    assert(v.size == 3);
-    pushBack(&v, 7);
-    assert(v.size == 4);
-    assert(v.capacity == 6);
-}
-
-void test_popBack_emptyVector() {
-    vector v = createVector(0);
-    popBack(&v);
-}
-
-void test_popBack_notEmptyVector() {
-    vector v = createVector(0);
-    pushBack(&v, 10);
-    assert(v.size == 1);
-    popBack(&v);
-    assert(v.size == 0);
-    assert(v.capacity == 1);
-}
-
-void test_atVector_notEmptyVector() {
-    vector v = createVector(5);
-    pushBack(&v, 10);
-    pushBack(&v, 7);
-    pushBack(&v, 8);
-    pushBack(&v, 9);
-    pushBack(&v, 15);
-    int *index = atVector(&v, 3);
-    assert(*index == 9);
-}
-
-void test_atVector_requestToLastElement(){
-    vector v = createVector(5);
-    pushBack(&v, 10);
-    pushBack(&v, 7);
-    pushBack(&v, 8);
-    pushBack(&v, 9);
-    pushBack(&v, 15);
-    int *index = atVector(&v, 4);
-    assert(*index == 15);
-}
-
-void test_back_oneElementInVector(){
-    vector v = createVector(1);
-    pushBack(&v, 10);
-    int *index = back(&v);
-    assert(*index == 10);
-}
-void test_front_oneElementInVector(){
-    vector v = createVector(1);
-    pushBack(&v, 10);
-    int *index = front(&v);
-    assert(*index == 10);
-}
-
-void test() {
-    test_pushBack_emptyVector();
-    test_pushBack_fullVector();
-    test_popBack_emptyVector();
-    test_popBack_notEmptyVector();
-    test_atVector_notEmptyVector();
-    test_atVector_requestToLastElement();
-    test_back_oneElementInVector();
-    test_front_oneElementInVector();
-}
-
-int main() {
-    test();
-}
-
