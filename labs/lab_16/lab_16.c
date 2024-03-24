4dc23622302c22c1e99c15fcf47edbc765ee0a0a
McLaren95 <maksimkruglikov1@gmail.com>
2024-03-24 10:58:38 +0300

    exercise 1 finished

--- labs/lab_16/lab_16.c
+++ labs/lab_16/lab_16.c
@@ -5,19 +5,3 @@ void exercise_1(matrix m) {
     position max_pos = getMaxValuePos(m);
     swapRows(m, min_pos.rowIndex, max_pos.rowIndex);
 }
-
-void exercise_2(matrix m) {
-    insertionSortRowsMatrixByRowCriteria(m, getMax);
-}
-
-void exercise_3(matrix m) {
-    selectionSortColsMatrixByColCriteria(m, getMin);
-}
-
-void exercise_4(matrix m) {
-    if(isSymmetricMatrix(&m)){
-        squareMatrix(&m);
-    }else{
-        printf("This matrix is not symmetric\n");
-    }
-}
