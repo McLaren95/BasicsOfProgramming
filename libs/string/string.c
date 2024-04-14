410ab9c2bbdc8e59666e49b96fdaa5ae1f6b9b8e
McLaren95 <maksimkruglikov1@gmail.com>
2024-04-14 15:48:54 +0300

    strlen_ ready

---
+++ libs/string/string.c
@@ -0,0 +1,8 @@
+#include "string.h"
+
+size_t strlen_(const char *begin){
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
---
+++ libs/string/string.h
@@ -0,0 +1,11 @@
+#ifndef INC_STRING_H
+#define INC_STRING_H
+
+#include <stddef.h>
+
+
+//возвращает количество символов в строке (не считая ноль-символ)
+size_t strlen_(const char *begin);
+
+
+#endif
