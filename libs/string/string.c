933394f91cd7f4789ac1635d1634a4333b32a913
McLaren95 <maksimkruglikov1@gmail.com>
2024-04-14 15:58:42 +0300

    findNonSpace ready

--- libs/string/string.c
+++ libs/string/string.c
@@ -1,14 +1,21 @@
 #include "string.h"
 
-size_t strlen_(const char *begin){
+size_t strlen_(const char *begin) {
     char *end = begin;
     while (*end != '\0')
         end++;
     return end - begin;
 }
 
-char* find(char *begin, char *end, int ch){
+char *find(char *begin, char *end, int ch) {
     while (begin != end && *begin != ch)
         begin++;
+    return begin;
+}
+
+char *findNonSpace(char *begin) {
+    while (isspace(*begin) && *begin != '\0') {
+        begin++;
+    }
     return begin;
 }
